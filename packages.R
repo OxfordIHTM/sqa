c799a45e773c16940d708a97dc463a926204f5ee
Ernest Guevarra <ernest@guevarra.io>
2025-08-01 15:50:06 +0100

    setup repo with pdfs

--- packages.R
+++ packages.R
@@ -1,9 +1,6 @@
-################################################################################
-#
-# Load packages required for standard workflows
-#
-################################################################################
+# Load packages required for standard workflows --------------------------------
 
+## Standard packages for most workflows ----
 library(targets)
 library(tarchetypes)
 library(here)
@@ -15,4 +12,9 @@ library(tidyr)
 library(openxlsx)
 library(ggplot2)
 library(quarto)
+
+
+## Packages to work with PDF files ----
+library(pdftools)
+library(tesseract)
 
